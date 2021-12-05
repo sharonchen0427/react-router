124a0487bdd81c81440347e0dced8ec99c3d7c2f
-chenshumei <-chensharon0427@gmail.com>
2021-12-04 20:10:11 -0500

    format code

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,9 +3,7 @@ import React, { Component } from 'react'
 export default class Home extends Component {
     render() {
         return (
-             
-                 <h3>我是Home的内容</h3>
-             
+            <h3>我是Home的内容</h3>
         )
     }
 }
